5da9774d7ee5b79aa5ac16ccc91078893b42cf63
jackie <mvpbremah@yahoo.com>
2020-08-27 20:36:00 -0400

    commit

--- Develop/app.js
+++ Develop/app.js
@@ -9,6 +9,199 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+const { inherits } = require("util");
+const employeeList = [];
+
+buildEngineer = () => {
+
+  inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "Please provide engineer's name."
+    },
+    {
+      type: "input",
+      name: "id",
+      message: "Engineer's ID#?"
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "Engineer's email address?",
+    },
+    {
+      type: "input",
+      name: "github",
+      message: "Engineer's GitHub username?"
+
+    }
+  ])
+    .then(results => {
+      results.role = "Engineer";
+
+      const { name, id, email, github, role } = results;
+
+      const newEmployee = new Engineer(name, id, email, github, role)
+
+      employeeList.push(newEmployee);
+
+      console.log(`\n${name} has been added!\n`);
+
+      inquirer.prompt({
+        type: "list",
+        name: "addAnother",
+        message: "Would you like to add another employee?",
+        choices: [
+          'YES',
+          "NO"
+        ]
+      })
+        .then(choice => {
+          if (choice.addAnother === 'YES') {
+            buildEmployeeList();;
+          } else {
+            exitQuestions();
+          }
+        })
+    });
+
+}
+
+buildIntern = () => {
+
+  inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "Please provide intern's name."
+    },
+    {
+      type: "input",
+      name: "id",
+      message: "Intern's ID#?"
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "Intern's email address?",
+    },
+    {
+      type: "input",
+      name: "school",
+      message: "Intern's school?"
+
+    }
+  ])
+    .then(results => {
+      results.role = "Intern";
+
+      const { name, id, email, school, role } = results;
+
+      const newEmployee = new Intern(name, id, email, school, role)
+
+      employeeList.push(newEmployee);
+
+      console.log(`\n${name} has been added!\n`);
+
+      inquirer.prompt({
+        type: "list",
+        name: "addAnother",
+        message: "Would you like to add another employee?",
+        choices: [
+          'YES',
+          "NO"
+        ]
+      })
+        .then(choice => {
+          if (choice.addAnother === 'YES') {
+            buildEmployeeList();;
+          } else {
+            exitQuestions();
+          }
+        })
+    });
+
+}
+
+buildEmployeeList = () => {
+
+  inquirer.prompt({
+    type: "list",
+    name: "employeeType",
+    message: "Choose employee type",
+    choices: [
+      'Engineer',
+      'Intern'
+    ]
+  })
+    .then(choice => {
+      if (choice.employeeType === 'Engineer') {
+        buildEngineer();
+      } else {
+        buildIntern();
+      }
+    })
+
+};
+
+
+init = () => {
+  console.log("\nWelcome to the employee page builder!");
+  console.log("You're just a few questions away from your new employee summary page.\n");
+
+  inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "Please provide Manager's name."
+    },
+    {
+      type: "input",
+      name: "id",
+      message: "Manager's ID#?"
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "Manager's email address?",
+    },
+    {
+      type: "input",
+      name: "officeNumber",
+      message: "This team's office number?"
+
+    }
+  ])
+    .then(results => {
+      results.role = "Manager";
+
+      const { name, id, email, officeNumber, role } = results;
+
+      const newEmployee = new Manager(name, id, email, officeNumber, role)
+
+      employeeList.push(newEmployee);
+
+      console.log("\nGreat! Now let's start adding employees! \n");
+
+      buildEmployeeList();
+
+    });
+
+}
+
+exitQuestions = () => {
+  console.log(employeeList);
+  render(employeeList);
+  console.log(render(employeeList));
+
+  fs.writeFile(outputPath, render(employeeList), function (err) {
+    if (err) return console.log(err);
+
+  });
+}
+
+init()
 
 
 // Write code to use inquirer to gather information about the development team members,
